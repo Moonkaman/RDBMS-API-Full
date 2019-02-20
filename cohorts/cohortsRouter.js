7d8068240b7985bf37f763fd46a76ea218da504c
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-20 15:27:21 -0700

    Create post endpoint in cohorts router

--- cohorts/cohortsRouter.js
+++ cohorts/cohortsRouter.js
@@ -10,4 +10,18 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({errorMessage: 'Could not retrieve list of cohorts at this time.', error: err}));
 })
 
+router.post('/', (req, res) => {
+  if(req.body.name){
+    db('cohorts').insert(req.body)
+      .then(id => {
+        db('cohorts').where({id: id[0]}).first()
+          .then(newCohort => res.status(201).json(newCohort))
+          .catch(err => res.status(500).json({errorMessage: 'Could not retrieve the new cohort at this time.', error: err}));
+      })
+      .catch(err => res.status(500).json({errorMessage: 'Could not create a new cohort at this time', error: err}));
+  } else {
+    res.status(400).json({errorMessage: 'Please provide a name with the request'});
+  }
+})
+
 module.exports = router;
